0043f776a984cc3aa43d654c87eee867bdd2e9da
Alexander Fang <alex.s.fang@gmail.com>
2019-11-13 17:04:06 -0600

    example line chart from react-native-chart-kit

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import React, { useState , useEffect} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import json from './public/data/assignments.json';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +18,75 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+const Graph = ({ state }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Bezier Line Chart</Text>
+      {/* TODO: CONVERT TO BAR, GET IT WORKING WITH OUR DATA */}
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel={"$"}
+        yAxisSuffix={"k"}
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        // bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  const [classes, setClasses] = useState([{id: "", title: "", assignments: []}])
+  const [allClasses, setAllClasses] = useState([{id: "", title: "", assignments: []}])
+  const url = '/data/assignments.json';
+
+  useEffect(() => {
+    const fetchClasses= async () => {
+      // const response = await fetch(url);
+      // if (!response.ok) throw response;
+      // const json = await response.json();
+      setAllClasses(json.courses);
+      let userCourses = json.users[0].courses;
+      setClasses(json.courses.filter(course => userCourses.includes(course.id)));
+    }
+    fetchClasses();
+  }, []);
+
+  return (<Graph key={classes.title} state={{classes, setClasses}}></Graph>);
+}
